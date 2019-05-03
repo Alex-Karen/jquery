fb63e961ee6116e2c530abe981eb6c914b0eed3a
xfh <m13928810941@163.com>
2019-05-03 14:09:56 +0800

    add 2019-05-03

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# Jquery学习
+# jQuery学习
+
+jQuery精髓：选择元素    循环操作    链式调用
+
+## 选择器
+
+    css selector
+
+    jquery unique selector
+
+    null、undefined、dom
+
+    $(function(){}) $(document).ready(function())
+
+## DOM
+
+$('.class').get()  //获得由选择器指定的 DOM 元素
+
+$('.class').eq()   //将匹配元素集缩减值指定 index 上的一个
+
+$('.class').find()  //获得当前元素集合中每个元素的后代,通过选择器、jQuery 对象或元素来筛选 向下查找
+
+$('.class').find('li').filter('.demo')  //将匹配元素集合缩减为匹配指定选择器的元素
+
+$('.class').find('li').not('.demo')  //从匹配元素集合中删除元素  filter反选
+
+$('.li').has('ul').css()    //将匹配元素集合缩减为包含特定元素的后代的集合。 含有ul的li
+
+$('.wrapper').is('span')    //根据选择器检查当前匹配元素集合，如果存在至少一个匹配元素，则返回 true。
+
+$('.wrapper').add('ul')   //将元素添加到匹配元素的集合中
+
+$('.wrapper').end()   //结束当前链条中的最近的筛选操作，并将匹配元素集还原为之前的状态
+
+## 赋值取值方法
+
+.html() 返回或设置被选元素的内容 (inner HTML)
+
+.text() 设置或返回被选元素的文本内容
+
+.size() 返回被 jQuery 选择器匹配的元素的数量 $().lenght
+
+.addClass()
+
+.removeClass()
+
+.hasClass()
+
+.css()
+
+.attr()
+
+.prop()
+
+    尽量避免直接添加行间样式，因为其权重过高 ，这样不利于响应式设计，破坏了c3+h5优雅的解决方案
+
+    attr和prop的区别：jQuery认为：attribute的checked、selected、disabled就是表示该属性初始状态的值，
+
+    property的checked、selected、disabled才表示该属性实时状态的值(值为true或false)
+
+.val()
---
+++ index.js
@@ -0,0 +1,77 @@
+(function() {
+    function jQuery(selector) {
+        return new jQuery.prototype.init(selector);
+    }
+    jQuery.prototype.init = function (selector) {
+        this.length = 0;
+        if (selector == null) {
+            return null;
+        }
+        if (typeof selector == 'string' && selector.indexOf('.') != -1) {
+            var dom = document.getElementsByClassName(selector.slice(1));
+        } else if(typeof selector == 'string' && selector.indexOf('#') != -1){
+            var dom = document.getElementById(selector.slice(1));
+        }
+        if (selector instanceof Element || dom.length == undefined) {
+            this[0] = dom || selector;
+            this.length++;
+        } else {
+            for(var i = 0; i < dom.length; i++) {
+                this[i] = dom[i];
+                this.length++;
+            }
+        }
+    }
+    jQuery.prototype.css = function(config) {
+        for (var i = 0; i < this.length; i++) {
+            for (attr in config) {
+                this[i].style[attr] = config[attr];
+            }
+        }
+        return this;
+    }
+    jQuery.prototype.pushStack = function(dom) {
+        if (dom.constructor != jQuery) {
+            dom = jQuery(dom);
+        }
+        dom.prevObject = this; 
+        return dom;
+    }
+    jQuery.prototype.get = function(num) {
+        return num != null ? (num >= 0 ? this[num] : this[num + this.length]) : [].slice.call(this, 0);
+    }
+    jQuery.prototype.eq = function(num) {
+        var dom = num != null ? (num >= 0 ? this[num] : this[num + this.length]) : null;
+        return this.pushStack(dom);
+    }
+    jQuery.prototype.add = function(selector) {
+        var curtObj = jQuery(selector);
+        var baseObj = this;
+        var newObj = jQuery();
+
+        for(var i = 0; i < curtObj.length; i++) {
+            newObj[newObj.length++] = curtObj[i];
+        }
+        for(var i = 0; i < baseObj.length; i++) {
+            newObj[newObj.length++] = baseObj[i];
+        }
+        
+        this.pushStack(newObj);
+        return newObj;
+    }
+    jQuery.prototype.end = function() {
+        return this.prevObject;
+    }
+    jQuery.prototype.init.prototype = jQuery.prototype;
+    window.$ = window.jQuery = jQuery;
+})();
+// $('.demo').css({width:'100px',height: '100px', backgroundColor: 'red'})
+// $('.demo').get(0);
+// $('.demo').eq(0);
+// $('.wrapper ul').find('li')
+// $('.wrapper ul li').filter(function(index, ele) {
+//     return index % 2 == 0;
+// })
+// $('.wrapper ul').find('li').is('.demo')
+// $('.wrapper').add('.demo').css({width:'100px', height: '100px',backgoroundColor: 'red'});
+// $('.wrapper').find('ul').css().end();
